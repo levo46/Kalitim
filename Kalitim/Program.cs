cbb010a4e437a838acaf8264dd7f45c5d583f349
levent581 <levent581@BPC03>
2023-12-14 14:53:12 +0300

    Proje dosyası ekle.

---
+++ Kalitim/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalitim
+{
+    class OkulPersoneli
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+    }
+    class Ogretmenler : OkulPersoneli
+    {
+        public string brans { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogretmenler ogretmenler = new Ogretmenler
+            {
+                Ad = "Nuri",
+                Soyad = "Tıraş",
+                brans = "Bilişim"
+            };
+            OkulPersoneli per = ogretmenler;
+            Console.WriteLine(per.Ad);
+            Console.WriteLine(per.Soyad);
+        }
+    }
+}
